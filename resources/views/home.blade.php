4454210aec3fc84b66af4df3459a79c1380ae98e
Michele Ianni <150182209+MicheleIanni86@users.noreply.github.com>
2024-03-15 16:19:56 +0100

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="ie=edge" http-equiv="X-UA-Compatible">
+    <title>Document</title>
+
+    @vite('resources/js/app.js')
+  </head>
+
+  <body>
+    <h1>Laravel with Bootstrap + Vite</h1>
+  </body>
+
+</html>
